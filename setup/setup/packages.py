fc46e814c987aedeab4ef5dc84ef8dbc540ee400
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-26 01:04:31 +0100

    update

--- setup/setup/packages.py
+++ setup/setup/packages.py
@@ -7,11 +7,26 @@ def install_packages_from_file(file_path):
     install_packages(packages)
 
 def install_packages(packages):
-    subprocess.run(["sudo", "paru", "-S", "--noconfirm", *packages], check=True)
+    subprocess.run(["paru", "-S", "--noconfirm", *packages], check=True)
 
 def uninstall_packages_from_file(file_path):
     packages = parse_file_entries(file_path)
     uninstall_packages(packages)
 
 def uninstall_packages(packages):
-    subprocess.run(["sudo", "pacman", "-Rns", "--noconfirm", *packages], check=True)
+    installed_packages = get_installed_packages_subset(packages)
+    if installed_packages:
+        subprocess.run(
+            ["sudo", "pacman", "-Rns", "--noconfirm", *installed_packages],
+            check=True
+        )
+
+def get_installed_packages_subset(packages):
+    result = subprocess.run(
+        ["pacman", "-Qq", *packages],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    installed_packages = result.stdout.strip().split('\n')
+    return installed_packages
