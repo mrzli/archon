b1eb3a8418a52493de3a862bc3a01e2da683fde2
Goran Mržljak <goran.mrzljak@gmail.com>
2025-12-17 21:20:24 +0100

    update

--- setup/setup/services.py
+++ setup/setup/services.py
@@ -7,7 +7,7 @@ def enable_and_start_services_from_file(services_file, is_user_service=False):
     enable_and_start_services(services, is_user_service=is_user_service)
 
 def enable_and_start_services(services, is_user_service=False):
-    cmd = get_command(services, "enable", is_user_service=is_user_service)
+    cmd = get_services_command(services, "enable", is_user_service=is_user_service)
     subprocess.run(cmd, check=True)
 
 def disable_and_stop_services_from_file(services_file, is_user_service=False):
@@ -15,10 +15,12 @@ def disable_and_stop_services_from_file(services_file, is_user_service=False):
     disable_and_stop_services(services, is_user_service=is_user_service)
 
 def disable_and_stop_services(services, is_user_service=False):
-    cmd = get_command(services, "disable", is_user_service=is_user_service)
-    subprocess.run(cmd, check=True)
+    services_to_uninstall = get_services_to_uninstall(services, is_user_service=is_user_service)
+    if services_to_uninstall and len(services_to_uninstall) > 0:
+        cmd = get_services_command(services_to_uninstall, "disable", is_user_service=is_user_service)
+        subprocess.run(cmd, check=True)
 
-def get_command(services, action, is_user_service=False):
+def get_services_command(services, action, is_user_service=False):
     return [
         *(["sudo"] if not is_user_service else []),
         "systemctl",
@@ -27,3 +29,37 @@ def get_command(services, action, is_user_service=False):
         "--now",
         *services
     ]
+
+def get_services_to_uninstall(services, is_user_service=False):
+    existing_services = get_existing_services(is_user_service=is_user_service)
+    services_to_uninstall = [
+        service
+        for service in services
+        if service in existing_services
+    ]
+    print(f"Services to uninstall: {services_to_uninstall}")
+    return services_to_uninstall
+
+def get_existing_services(is_user_service=False):
+    cmd = [
+        *(["sudo"] if not is_user_service else []),
+        "systemctl",
+        *(["--user"] if is_user_service else []),
+        "list-unit-files",
+        "--type=service",
+        "--no-pager",
+        "--no-legend"
+    ]
+    result = subprocess.run(
+        cmd,
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    services = [
+        line.split()[0]
+        for line in (result.stdout or "").splitlines()
+        if line.strip()
+    ]
+    print(f"Existing services: {services}")
+    return services
