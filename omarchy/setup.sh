756eee11232713aaccaf6fa42dd564963882b114
Goran Mržljak <goran.mrzljak@gmail.com>
2025-12-20 11:13:25 +0100

    update

--- omarchy/setup.sh
+++ omarchy/setup.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-echo "Setting up configuration..."
+set -euo pipefail
+
+# Do this manually:
+# - Log into Gmail and YouTube on Chromium.
+# - Log into Grok.
+
+# Remove unneeded.
+
+packages_to_remove=(
+	1password
+  aether
+  obsidian
+  spotify
+  typora
+)
+
+for pkg in "${packages_to_remove[@]}"; do
+	omarchy-pkg-remove "$pkg"
+done
+
+omarchy-webapp-remove Basecamp Fizzy HEY
+
+# Install needed.
+
+omarchy-install-steam
