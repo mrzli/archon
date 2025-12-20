781559fc93fc9797a55ecc3dade08dd1451a4d4f
Goran Mržljak <goran.mrzljak@gmail.com>
2025-12-20 11:37:34 +0100

    update

--- omarchy/setup.sh
+++ omarchy/setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# Prerequisites.
+
 set -euo pipefail
 
-# Do this manually:
-# - Log into Gmail and YouTube on Chromium.
-# - Log into Grok.
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+mkdir -p "$XDG_CONFIG_HOME"
 
 # Remove unneeded.
 
@@ -30,3 +31,29 @@ omarchy-webapp-remove "${webapps_to_remove[@]}"
 # Install needed.
 
 omarchy-install-steam
+omarchy-install-vscode
+
+# Install VSCode extensions.
+vscode_extensions_file="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/data/vscode-extensions.txt"
+mapfile -t vscode_extensions < <(grep -Ev '^\s*(#|$)' "$vscode_extensions_file")
+
+for extension in "${vscode_extensions[@]}"; do
+  code --install-extension "$extension" --force
+done
+
+# Setup configs.
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+config_source_dir="$script_dir/data/config"
+
+for src in "$config_source_dir"/*; do
+  [[ -d "$src" ]] || continue
+
+  name="$(basename "$src")"
+  rm -rf "$XDG_CONFIG_HOME/$name"
+  cp -R "$src" "$XDG_CONFIG_HOME/$name"
+done
+
+# Do this manually:
+# - Log into Gmail and YouTube on Chromium.
+# - Log into Grok.
